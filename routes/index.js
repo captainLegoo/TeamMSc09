bb31c4cf3dacb3b5e29919c028bd774b298cdad6
Liang-Yu Huang <lhuang50@sheffield.ac.uk>
2024-03-12 02:04:43 +0000

    Add fake frontend, Update add/edit/view

--- routes/index.js
+++ routes/index.js
@@ -1,109 +1,125 @@
 var express = require('express');
 var router = express.Router();
 
-const mongoose = require('mongoose');
+const url = require('url');
+const PlantModel = require('../model/PlantModel');
 const {Double} = require("mongodb");
-const uri = "mongodb+srv://lhuang50:huang123_@cluster0.ihtm3iq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-mongoose.connect(uri)
+const URI = "mongodb+srv://lhuang50:huang123_@cluster0.ihtm3iq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const mongoose = require('mongoose');
+mongoose.connect(URI)
     .then((result)=> console.log('connect successfully'))
     .catch((error) => console.log(error))
 
-const Schema = mongoose.Schema;
-
-const plantSchema = new Schema({
-  name: String,
-  author: String,
-  geolocation: String,
-  description: String,
-  outward:[{height: Number, size:Number,distribution:String}],
-  features:[{hasFlower: Boolean, hasLeaf:Boolean, hasSeed:Boolean,Sun:String,FlowerColor:String}],
-  tags:[{name:String,identify:String}],
-  pictureUrl:String
-});
-
-const plant = mongoose.model('plant', plantSchema);
+// ADD PLANT
 router.post('/addPlant',function (req,res){
-  const newPlant = new plant({
-    title: req.body.title,
-    author: req.body.author,
-    geolocation: req.body.geolocation,
-    description: req.body.description,
-    outward: [{
-      height: req.body.height,
-      size: req.body.size,
-      distribution: req.body.distribution
-    }],
-    features: [{
-      hasFlower: req.body.hasFlower,
-      hasLeaf: req.body.hasLeaf,
-      hasSeed: req.body.hasSeed,
-      Sun: req.body.Sun,
-      FlowerColor: req.body.FlowerColor
-    }],
-    tags: [{
-      name: req.body.name,
-      identify: req.body.tagIdentify
-    }],
-    pictureUrl: req.body.pictureUrl
+  const plant = new PlantModel({
+    date: new Date(),
+    location: {
+      coordinates: [req.body.latitude, req.body.longitude]
+    },
+    description : req.body.description,
+    plantSize : req.body.plantSize,
+    haveFlower : (req.body.haveFlower === 'true'),
+    haveLeaves : (req.body.haveLeaves === 'true'),
+    haveSeeds : (req.body.haveSeeds === 'true'),
+    sunExposure : req.body.sunExposure,
+    flowerColor : req.body.flowerColor,
+    identification : {
+      name : req.body.id_name,
+      status : req.body.id_status,
+      suggestedNames : req.body.id_suggestedNames,
+      dbpediaInfo : {
+        commonName : req.body.id_info_commonName,
+        scientificName : req.body.id_info_scientificName,
+        description : req.body.id_info_description,
+        uri : req.body.id_info_uri,
+      }
+    },
+    photo : req.body.photo,
+    userNickname : req.body.userNickname
   });
-  newPlant.save()
-      .then(() => {
-        console.log('New plant record created successfully!');
-      })
-      .catch((err) => {
-        console.error('Error creating plant record:', err);
-      });
+
+  plant.save()
+    .then(() => {
+      console.log('New plant record created successfully!');
+      res.render('index',{title:'successfully added'})
+    })
+    .catch((err) => {
+      console.error('Error creating plant record:', err);
+      res.render('index',{title:'failed to add'});
+    });
+
 })
 
-router.post('/updatePlant',async function (req, res) {
-  const plantId = req.body.id;
-  const existingPlant = await plant.findById(plantId);
-  if (!existingPlant) {
+// UPDATE PLANT
+router.post('/updatePlant', async (req, res)=>{
+  const _id = req.body._id;
+  const plant = await PlantModel.findById(_id);
+  if (!plant) {
     return res.status(404).json({message: 'Plant not found'});
   }
+  plant.location.coordinates = [req.body.latitude, req.body.longitude];
+  plant.description = req.body.description;
+  plant.plantSize = req.body.plantSize;
+  plant.haveFlower = (req.body.haveFlower==='true');
+  plant.haveLeaves = (req.body.haveLeaves==='true');
+  plant.haveSeeds = (req.body.haveSeeds==='true');
+  plant.sunExposure = req.body.sunExposure;
+  plant.flowerColor = req.body.flowerColor;
+  plant.identification.name = req.body.id_name;
+  plant.identification.status = req.body.id_status;
+  plant.identification.suggestedNames = req.body.id_suggestedNames;
+  plant.identification.dbpediaInfo.commonName = req.body.id_info_commonName;
+  plant.identification.dbpediaInfo.scientificName = req.body.id_info_scientificName;
+  plant.identification.dbpediaInfo.description = req.body.id_info_description;
+  plant.identification.dbpediaInfo.uri = req.body.id_info_uri;
+  plant.photo = req.body.photo;
+  plant.userNickname = req.body.userNickname;
 
-  existingPlant.title = updatedPlantData.title;
-  existingPlant.author = updatedPlantData.author;
-  existingPlant.geolocation = updatedPlantData.geolocation;
-  existingPlant.description = updatedPlantData.description;
-  existingPlant.outward = {
-    height: updatedPlantData.height,
-    size: updatedPlantData.size,
-    distribution: updatedPlantData.distribution,
-  };
-  existingPlant.features = {
-    hasFlower: updatedPlantData.hasFlower,
-    hasLeaf: updatedPlantData.hasLeaf,
-    hasSeed: updatedPlantData.hasSeed,
-    Sun: updatedPlantData.Sun,
-    FlowerColor: updatedPlantData.FlowerColor,
-  };
-  existingPlant.tags = {
-    name: updatedPlantData.name,
-    identify: updatedPlantData.tagIdentify,
-  };
-  existingPlant.pictureUrl = updatedPlantData.pictureUrl;
-
-  // Save the updated plant record
-  await existingPlant.save();
+  await plant.save();
+  res.redirect('/');
 })
 
 router.get('/plants', async (req, res) => {
   try {
-    const allPlants = await plant.find({});
-    res.status(200).json(allPlants);
+    const allPlants = await PlantModel.find({});
+    res.send(allPlants);
+    //res.status(200).json(allPlants);
   } catch (error) {
     console.error('Error fetching plant records:', error);
     res.status(500).json({ message: 'Internal server error' });
   }
 });
 
-
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Randy' });
+// /* GET home page. */
+router.get('/', async (req, res) => {
+  try {
+    const allPlants = await PlantModel.find({});
+    res.render('index',{data:allPlants,title:'randy'});
+  } catch (error) {
+    console.error('Error fetching plant records:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 });
+
+router.get('/edit',async (req,res) => {
+  const _id = req.params._id;
+  const urlObj = url.parse(req.url, true);
+  const query = urlObj.query;
+  const id = query._id;
+  console.log(query._id);
+  try {
+    const plant = await PlantModel.findById(id);
+    if (!plant) {
+      return res.status(404).json({message: 'Plant not found'});
+    }
+
+    res.render('edit',{title:'aaa',data:plant});
+  } catch (error) {
+    console.error('Error fetching plant records:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+})
 
 
 
