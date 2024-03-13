7a2caed2978dd36c3cda89bb8e91f0d0abe8e924
acp23cd <cdeng7@sheffield.ac.uk>
2024-03-13 14:41:12 +0000

    add home page connect to mongodb

--- routes/home.js
+++ routes/home.js
@@ -3,6 +3,21 @@ var router = express.Router();
 var PlantModel = require('../model/PlantModel');
 const IdGenerator = require('../utils/IdGenerator');
 const generator = new IdGenerator(1, 1);
+const connectToDatabase = require('../db/db');
+
+// 定义成功连接的回调函数
+function onDatabaseConnect() {
+    console.log("Successfully connected to the database");
+    // 在这里可以继续编写你的逻辑
+}
+
+// 定义连接失败的回调函数
+function onDatabaseError() {
+    console.error("Failed to connect to the database");
+}
+// 调用数据库连接函数，并传递成功和失败的回调函数
+connectToDatabase(onDatabaseConnect, onDatabaseError);
+
 
 router.get('/', (req, res, next) => {
     var userId = req.cookies.userId;
@@ -10,7 +25,7 @@ router.get('/', (req, res, next) => {
         userId = generator.getId();
         res.cookie("userId", userId, {maxAge: 365 * 24 * 60 * 60 * 1000});
     }
-    // console.log("userId: " + userId);
+    console.log("userId: " + userId);
 
     // PlantModel.find()
     //     .sort({date: -1})
