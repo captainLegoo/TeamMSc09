9e590510e787622aad5a5a99ab3dbb3e326cccda
acp23cd <cdeng7@sheffield.ac.uk>
2024-03-15 14:18:22 +0000

    request render home page

--- routes/home.js
+++ routes/home.js
@@ -5,17 +5,12 @@ const IdGenerator = require('../utils/IdGenerator');
 const generator = new IdGenerator(1, 1);
 const connectToDatabase = require('../db/db');
 
-// 定义成功连接的回调函数
 function onDatabaseConnect() {
     console.log("Successfully connected to the database");
-    // 在这里可以继续编写你的逻辑
 }
-
-// 定义连接失败的回调函数
 function onDatabaseError() {
     console.error("Failed to connect to the database");
 }
-// 调用数据库连接函数，并传递成功和失败的回调函数
 connectToDatabase(onDatabaseConnect, onDatabaseError);
 
 
@@ -41,7 +36,7 @@ router.get('/', (req, res, next) => {
     //     }
     // });
 
-    res.send("/home");
+    res.render("home");
 });
 
 module.exports = router;
