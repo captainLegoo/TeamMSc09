32e5ac2138eeb8b335afae1b6e8231aa43a630e0
acp23cd <cdeng7@sheffield.ac.uk>
2024-03-15 14:17:38 +0000

    add route

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var homeRouter = require('./routes/home');
+var modifyRouter = require('./routes/modify');
 var overviewRouter = require('./routes/overview');
 var singlePlantRouter = require('./routes/singlePlant');
 
 var app = express();
+app.use(express.static('public'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/modify', modifyRouter);
 app.use('/home', homeRouter);
 app.use('/overview', overviewRouter);
 app.use('/singlePlant', singlePlantRouter);
