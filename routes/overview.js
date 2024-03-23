7ca8eb5af77be72e26c83ddd242d8b2e4e36a56b
acp23cd <cdeng7@sheffield.ac.uk>
2024-03-23 23:06:51 +0800

    async get all plant data from database

--- routes/overview.js
+++ routes/overview.js
@@ -2,26 +2,20 @@ var express = require('express');
 var router = express.Router();
 var PlantModel = require('../model/PlantModel');
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
     var userId = req.cookies.userId;
     if (userId == null) {
         res.redirect('/home');
     }
 
-    // PlantModel.find()
-    //     .sort({date: -1})
-    //     .then((data) => {
-    //         console.log(data);
-    //         // Response successful
-    //         // res.render('home', { })
-    //     }).catch(err => {
-    //     if (err) {
-    //         res.status(500).send("Read Data failed");
-    //         return;
-    //     }
-    // });
-
-    res.render("overview");
+    try {
+        const data = await PlantModel.find().sort({date: -1});
+        // Response successful
+        res.render('overview', {plants: data});
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Read Data failed");
+    }
 });
 
 module.exports = router;
