8c1b509ad2870a8bcbd2281b74e65a4fa6c425c7
Haoyu Liang <hliang11@sheffield.ac.uk>
2024-04-02 19:23:26 +0100

    completed based functions in overview.ejs

--- routes/overview.js
+++ routes/overview.js
@@ -6,16 +6,42 @@ router.get('/', async (req, res, next) => {
     var userId = req.cookies.userId;
     if (userId == null) {
         res.redirect('/home');
+        return;
     }
 
+    let query = PlantModel.find();
+    const sort = req.query.sort;
+    const { lat, lng } = req.query;
+
+    // 检查排序参数来决定使用哪种排序
+    if (sort === 'date') {
+        // 按日期排序
+        query = query.sort({date: -1});
+    } else if (sort === 'name') {
+        // 按名称排序
+        query = query.sort({'identification.name': 1});
+    } else if (sort === 'distance' && lat && lng) {
+        // 按距离排序，前提是提供了经纬度
+        query = query.near({
+            center: {
+                type: 'Point',
+                coordinates: [parseFloat(lng), parseFloat(lat)]
+            },
+            spherical: true
+        });
+    }
+    // 移除默认排序的else部分
+
     try {
-        const data = await PlantModel.find().sort({date: -1});
-        // Response successful
+        const data = await query.exec();
         res.render('overview', {plants: data});
     } catch (err) {
         console.error(err);
         res.status(500).send("Read Data failed");
     }
 });
+
+
+
 
 module.exports = router;
