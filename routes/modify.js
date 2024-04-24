6ac475e676600dccda22207763f14c3caf24161c
Liang-Yu Huang <lhuang50@sheffield.ac.uk>
2024-04-24 17:03:26 +0100

    implement add/edit plants

--- routes/modify.js
+++ routes/modify.js
@@ -6,6 +6,7 @@ const PlantModel = require('../model/PlantModel');
 const {Double} = require("mongodb");
 const URI = "mongodb+srv://lhuang50:huang123_@cluster0.ihtm3iq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const mongoose = require('mongoose');
+const path = require('path');
 
 const SparqlEndpointFetcher = require('fetch-sparql-endpoint').SparqlEndpointFetcher;
 const DataFactory = require('rdf-data-factory').DataFactory;
@@ -30,47 +31,57 @@ mongoose.connect(URI)
 // ADD PLANT
 router.post('/addPlant',upload.single('photo'),function (req,res){
   let base64Image = '';
+  //console.log("222")
+  // console.log(req.file);
+  console.log(req.file)
   fs.readFile(req.file.path, (err, data) => {
     base64Image = data.toString('base64');
-  });
-
-  const plant = new PlantModel({
-    date: new Date(),
-    location: {
-      coordinates: [req.body.latitude, req.body.longitude]
-    },
-    description : req.body.description,
-    plantSize : req.body.plantSize,
-    haveFlower : (req.body.haveFlower === 'true'),
-    haveLeaves : (req.body.haveLeaves === 'true'),
-    haveSeeds : (req.body.haveSeeds === 'true'),
-    sunExposure : req.body.sunExposure,
-    flowerColor : req.body.flowerColor,
-    identification : {
-      name : req.body.id_name,
-      status : req.body.id_status,
-      suggestedNames : req.body.id_suggestedNames,
-      dbpediaInfo : {
-        commonName : req.body.id_info_commonName,
-        scientificName : req.body.id_info_scientificName,
-        description : req.body.id_info_description,
-        uri : req.body.id_info_uri,
-      }
-    },
-    photo : base64Image,
-    userNickname : req.body.userNickname
-  });
-
-  plant.save()
-    .then(() => {
-      console.log('New plant record created successfully!');
-      res.render('index',{title:'successfully added'})
-    })
-    .catch((err) => {
-      console.error('Error creating plant record:', err);
-      res.render('index',{title:'failed to add'});
+    const plant = new PlantModel({
+      date: new Date(),
+      location: {
+        coordinates: [req.body.latitude, req.body.longitude]
+      },
+      description : req.body.description,
+      plantSize : req.body.plantSize,
+      haveFlower : (req.body.haveFlower === 'true'),
+      haveLeaves : (req.body.haveLeaves === 'true'),
+      haveSeeds : (req.body.haveSeeds === 'true'),
+      sunExposure : req.body.sunExposure,
+      flowerColor : req.body.flowerColor,
+      identification : {
+        name : req.body.name,
+        status : req.body.id_status,
+        suggestedNames : req.body.id_suggestedNames,
+        dbpediaInfo : {
+          commonName : req.body.id_info_commonName,
+          scientificName : req.body.id_info_scientificName,
+          description : req.body.id_info_description,
+          uri : req.body.id_info_uri,
+        }
+      },
+      photo : base64Image,
+      userNickname : req.body.userNickname
     });
 
+    plant.save()
+        .then(() => {
+          console.log('New plant record created successfully!');
+          res.render('home',{title:'successfully added'})
+        })
+        .catch((err) => {
+          console.error('Error creating plant record:', err);
+          res.render('home',{title:'failed to add'});
+        });
+  });
+  // console.log(base64Image)
+
+
+
+
+})
+
+router.get('/addPlant',function (req,res){
+  res.render('addPlant')
 })
 
 // UPDATE PLANT
@@ -88,18 +99,19 @@ router.post('/updatePlant', async (req, res)=>{
   plant.haveSeeds = (req.body.haveSeeds==='true');
   plant.sunExposure = req.body.sunExposure;
   plant.flowerColor = req.body.flowerColor;
-  plant.identification.name = req.body.id_name;
+  plant.identification.name = req.body.name;
   plant.identification.status = req.body.id_status;
   plant.identification.suggestedNames = req.body.id_suggestedNames;
   plant.identification.dbpediaInfo.commonName = req.body.id_info_commonName;
   plant.identification.dbpediaInfo.scientificName = req.body.id_info_scientificName;
   plant.identification.dbpediaInfo.description = req.body.id_info_description;
   plant.identification.dbpediaInfo.uri = req.body.id_info_uri;
-  plant.photo = req.body.photo;
+  //plant.photo = req.body.photo;
   plant.userNickname = req.body.userNickname;
 
   await plant.save();
-  res.redirect('/');
+  //res.redirect('home');
+  res.render("home")
 })
 
 router.get('/plants', async (req, res) => {
@@ -126,13 +138,14 @@ router.get('/', async (req, res) => {
 
 router.get('/edit',async (req,res) => {
   const _id = req.params._id;
+  console.log("id is"+_id)
   const urlObj = url.parse(req.url, true);
   const query = urlObj.query;
   const id = query._id;
   console.log(query._id);
   try {
     //http://localhost:3000/modify/edit?_id=65ef932e7f392acbffefa873
-    const plant = await PlantModel.findById('65ef932e7f392acbffefa873');
+    const plant = await PlantModel.findById(query._id);
     if (!plant) {
       return res.status(404).json({message: 'Plant not found'});
     }
