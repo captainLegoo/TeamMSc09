5d9c9adb62f690f139417e970c95f2acf7690a9b
acp23cd <cdeng7@sheffield.ac.uk>
2024-04-26 12:22:45 +0100

    disable type

--- model/PlantModel.js
+++ model/PlantModel.js
@@ -7,11 +7,11 @@ let PlantSchema = new mongoose.Schema({
         required: true
     },
     location: {
-        type: {
-            type: String,
-            enum: ['Point'],
-            required: true
-        },
+        // type: {
+        //     type: String,
+        //     enum: ['Point'],
+        //     required: true
+        // },
         coordinates: {
             type: [Number],
             required: true
