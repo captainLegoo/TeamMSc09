9f6763efafbb50e05715bc7c2bde5e9748873356
acp23cd <cdeng7@sheffield.ac.uk>
2024-05-06 10:49:23 +0100

    save offline data to MongoDB

--- routes/mongo.js
+++ routes/mongo.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const mongoose = require('mongoose');
+const plantController = require("../controllers/plant_controller");
 
 router.get('/mongoStatus', (req, res, next) => {
     const status = mongoose.connection.readyState;
@@ -11,6 +12,23 @@ router.get('/mongoStatus', (req, res, next) => {
     // 2: connecting
     // 3: disconnecting
     res.json({ status });
+});
+
+router.post('/saveOfflineData', async (req, res, next) => {
+    const plantData = req.body.plant;
+    const { _id, ...dataToSend } = plantData;
+    console.log("plantData => ", dataToSend);
+    try {
+        const saveResult = await plantController.saveOfflineData(dataToSend);
+        if (saveResult.success) {
+            return res.status(200).json({ message: saveResult.message });
+        } else {
+            return res.status(500).json({ error: saveResult.error });
+        }
+    } catch (error) {
+        console.error("Error saving plant data:", error);
+        return res.status(500).json({ error: 'Failed to save plant data!' });
+    }
 });
 
 module.exports = router;
