465dec5e451b94c3615b3d0187e86d8aa8507c0a
acp23cd <cdeng7@sheffield.ac.uk>
2024-05-06 10:49:37 +0100

    unused

--- public/sw.js
+++ public/sw.js
@@ -34,69 +34,69 @@ self.addEventListener('install', event => {
 });
 
 //clear cache on reload
-self.addEventListener('activate', event => {
-// Remove old caches
-    event.waitUntil(
-        (async () => {
-            const keys = await caches.keys();
-            return keys.map(async (cache) => {
-                if(cache !== "static") {
-                    console.log('Service Worker: Removing old cache: '+cache);
-                    return await caches.delete(cache);
-                }
-            })
-        })()
-    )
-})
+// self.addEventListener('activate', event => {
+// // Remove old caches
+//     event.waitUntil(
+//         (async () => {
+//             const keys = await caches.keys();
+//             return keys.map(async (cache) => {
+//                 if(cache !== "static") {
+//                     console.log('Service Worker: Removing old cache: '+cache);
+//                     return await caches.delete(cache);
+//                 }
+//             })
+//         })()
+//     )
+// })
 
 // Fetch event to fetch from cache first
-self.addEventListener('fetch', event => {
-    event.respondWith((async () => {
-        const cache = await caches.open("static");
-        const cachedResponse = await cache.match(event.request);
-        if (cachedResponse) {
-            console.log('Service Worker: Fetching from Cache: ', event.request.url);
-            return cachedResponse;
-        }
-        console.log('Service Worker: Fetching from URL: ', event.request.url);
-        return fetch(event.request);
-    })());
-});
+// self.addEventListener('fetch', event => {
+//     event.respondWith((async () => {
+//         const cache = await caches.open("static");
+//         const cachedResponse = await cache.match(event.request);
+//         if (cachedResponse) {
+//             console.log('Service Worker: Fetching from Cache: ', event.request.url);
+//             return cachedResponse;
+//         }
+//         console.log('Service Worker: Fetching from URL: ', event.request.url);
+//         return fetch(event.request);
+//     })());
+// });
 
 //Sync event to sync the todos
-self.addEventListener('sync', event => {
-    if (event.tag === 'sync-plant') {
-        console.log('Service Worker: Syncing new Plants');
-        openSyncPlantsIDB().then((syncPostDB) => {
-            getAllSyncPlants(syncPostDB).then((syncPlants) => {
-                for (const syncPlant of syncPlants) {
-                    console.log('Service Worker: Syncing new Plant: ', syncPlant);
-                    console.log(syncPlant.text)
-                    // Create a FormData object
-                    const formData = new URLSearchParams();
-
-                    // Iterate over the properties of the JSON object and append them to FormData
-                    formData.append("text", syncPlant.text);
-
-                    // TODO Fetch with FormData instead of JSON
-                    fetch('http://localhost:3000/modify/addPlant', {
-                        method: 'POST',
-                        body: formData,
-                        headers: {
-                            'Content-Type': 'application/x-www-form-urlencoded',
-                        },
-                    }).then(() => {
-                        console.log('Service Worker: Syncing new Plant: ', syncPlant, ' done');
-                        deleteSyncTodoFromIDB(syncPostDB,syncTodo.id);
-                        // Send a notification
-                        self.registration.showNotification('Plant Synced', {
-                            body: 'Plant synced successfully!',
-                        });
-                    }).catch((err) => {
-                        console.error('Service Worker: Syncing new Plant: ', syncPlant, ' failed');
-                    });
-                }
-            });
-        });
-    }
-});
+// self.addEventListener('sync', event => {
+//     if (event.tag === 'sync-plant') {
+//         console.log('Service Worker: Syncing new Plants');
+//         openSyncPlantsIDB().then((syncPostDB) => {
+//             getAllSyncPlants(syncPostDB).then((syncPlants) => {
+//                 for (const syncPlant of syncPlants) {
+//                     console.log('Service Worker: Syncing new Plant: ', syncPlant);
+//                     console.log(syncPlant.text)
+//                     // Create a FormData object
+//                     const formData = new URLSearchParams();
+//
+//                     // Iterate over the properties of the JSON object and append them to FormData
+//                     formData.append("text", syncPlant.text);
+//
+//                     // TODO Fetch with FormData instead of JSON
+//                     fetch('http://localhost:3000/modify/addPlant', {
+//                         method: 'POST',
+//                         body: formData,
+//                         headers: {
+//                             'Content-Type': 'application/x-www-form-urlencoded',
+//                         },
+//                     }).then(() => {
+//                         console.log('Service Worker: Syncing new Plant: ', syncPlant, ' done');
+//                         deleteSyncTodoFromIDB(syncPostDB,syncTodo.id);
+//                         // Send a notification
+//                         self.registration.showNotification('Plant Synced', {
+//                             body: 'Plant synced successfully!',
+//                         });
+//                     }).catch((err) => {
+//                         console.error('Service Worker: Syncing new Plant: ', syncPlant, ' failed');
+//                     });
+//                 }
+//             });
+//         });
+//     }
+// });
