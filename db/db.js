71a26b6bde5ab64296c9189478f9a0ee18f41957
acp23cd <cdeng7@sheffield.ac.uk>
2024-05-11 14:38:47 +0100

    Complete the reconnection mechanism of mongodb

--- db/db.js
+++ db/db.js
@@ -1,23 +1,3 @@
-// const mongoose = require('mongoose');
-//
-// const {DBHOST, DBPORT, DBNAME} = require("../config/config");
-// const mongoDB = `mongodb://${DBHOST}:${DBPORT}/${DBNAME}`;
-// let connection;
-//
-// // Set Mongoose to use the global Promise library
-// mongoose.Promise = global.Promise;
-//
-// // Connect to the MongoDB server
-// mongoose.connect(mongoDB).then(result => {
-//     // Store the connection instance for later use if needed
-//     connection = result.connection;
-//     // Log a success message if the connection is established
-//     console.log("Connection Successful!");
-// }).catch(err => {
-//     // Log an error message if the connection fails
-//     console.log("Connection Failed!", err);
-// });
-
 const mongoose = require('mongoose');
 const retry = require('retry');
 
@@ -30,10 +10,10 @@ mongoose.Promise = global.Promise;
 
 function connectWithRetry() {
     const operation = retry.operation({
-        retries: 10,  // 重连的次数
-        factor: 2,    // 重连时间间隔的增长因子
-        minTimeout: 1000, // 最小的重连时间间隔
-        maxTimeout: 60000 // 最大的重连时间间隔
+        retries: 999999,
+        factor: 2,
+        minTimeout: 1000,
+        maxTimeout: 60000
     });
 
     operation.attempt(() => {
