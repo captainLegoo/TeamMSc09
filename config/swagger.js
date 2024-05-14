64e166b08b83a4a84777ad189d2ca0082e12ef5f
acp23cd <cdeng7@sheffield.ac.uk>
2024-05-14 13:14:51 +0100

    add swagger dependency and config file

---
+++ config/swagger.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const express = require('express')
+const swaggerUI = require('swagger-ui-express')
+const swaggerDoc = require('swagger-jsdoc')
+//配置swagger-jsdoc
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Plant API',
+            version: '1.0.0',
+            description: 'A basic application to discover and share plant sightings',
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000',
+            },
+        ],
+    },
+    apis: ['./routes/*.js'],
+}
+var swaggerJson = function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+}
+const swaggerSpec = swaggerDoc(options)
+
+var swaggerInstall = function(app) {
+    if (!app){
+        app = express()
+    }
+    // 开放相关接口，
+    app.get('/swagger.json', swaggerJson);
+    // 使用 swaggerSpec 生成 swagger 文档页面，并开放在指定路由
+    app.use('/swagger', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+}
+module.exports = swaggerInstall
